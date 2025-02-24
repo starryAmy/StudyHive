fa33452b645bfccdba5d14d6bab79117f1f78dfc
rayz arain <79001879+authorminator@users.noreply.github.com>
2025-02-24 19:31:31 +0900

    sticky notes on the desk show page - in progress

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,7 @@ import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
 import LoadMoreController from "controllers/load_more_controller";
 import MessageController from "controllers/message_controller";
 import BlackboardController from "controllers/blackboard_controller";
+import FollowController from "controllers/follow_controller";
 eagerLoadControllersFrom("controllers", application);
 application.register("load-more", LoadMoreController);
 application.register("message", MessageController);
