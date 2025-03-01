df215a85326fc5a8a2a54cc09f98ff7d1efa864c
Ruben HedstrÃom <ruben.hedstrom@hotmail.com>
2025-03-01 17:34:25 +0900

    using turbo streams

--- app/javascript/controllers/reset_form_controller.js
+++ app/javascript/controllers/reset_form_controller.js
@@ -14,23 +14,4 @@ export default class extends Controller {
     event.preventDefault()
     this.element.requestSubmit() // Submits the form programmatically
   }
-
-  updateLastOnline() {
-    fetch("/update_user_last_online", {
-      method: "POST",
-      headers: {
-        "X-CSRF-Token": document.querySelector("meta[name='csrf-token']").content,
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({})
-    }).then(response => {
-      if (response.ok) {
-        console.log("Last online updated");
-      }
-    }).catch(error => console.error("Error updating last online:", error));
-  }
-
-  startPolling() {
-    this.pollingInterval = setInterval(() => this.updateLastOnline(), 2000);
-  }
 }
