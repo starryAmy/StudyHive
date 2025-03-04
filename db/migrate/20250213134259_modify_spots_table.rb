b16997d80c67b20bff9f73af1be4fc3129783a7b
amy huang <flower850603@gmail.com>
2025-03-04 22:17:58 +0900

    change the style of desk to padding bottom

--- db/migrate/20250213134259_modify_spots_table.rb
+++
@@ -1,6 +0,0 @@
-class ModifySpotsTable < ActiveRecord::Migration[7.1]
-  def change
-    remove_reference :spots, :desk, foreign_key: true
-    add_reference :spots, :user, foreign_key: true
-  end
-end
--- db/migrate/20250301052825_add_intro_to_desks.rb
+++
@@ -1,5 +0,0 @@
-class AddIntroToDesks < ActiveRecord::Migration[7.1]
-  def change
-    add_column :desks, :intro, :text
-  end
-end
