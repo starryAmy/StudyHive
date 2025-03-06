652952b663aa43053b6a9caef74f176b864853f7
rayz arain <79001879+authorminator@users.noreply.github.com>
2025-03-06 22:27:08 +0900

    ai chat styling done

--- app/javascript/controllers/toggle_ai_controller.js
+++ app/javascript/controllers/toggle_ai_controller.js
@@ -2,12 +2,14 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="toggle-ai"
 export default class extends Controller {
+  static targets = ["aiChat"];
+
   connect() {
     console.log("Connected to toggle-ai controller");
-    this.messagesTarget.classList.add("d-none");
+    // this.aiChatTarget.classList.add("d-none");
   }
 
-  showHideMessages() {
-    this.messagesTarget.classList.toggle("d-none");
+  showHideAI() {
+    this.aiChatTarget.classList.toggle("d-none");
   }
 }
