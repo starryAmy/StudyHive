6f87394258b8ea5b080ea41cfd7177262816618b
rayz arain <79001879+authorminator@users.noreply.github.com>
2025-03-06 21:02:37 +0900

    ai chat is functional

--- app/services/openai_service.rb
+++ app/services/openai_service.rb
@@ -1,6 +1,6 @@
 class OpenaiService
   def initialize
-    @client = Openai::Client.new
+    @client = OpenAI::Client.new
   end
 
   def generate_response(message)
@@ -9,18 +9,15 @@ class OpenaiService
   #     prompt: prompt,
   #     max_tokens: 100
   #   )
-  puts '-' * 50
-  puts "Generating the response..."
-  puts '-' * 50
-  response = client.chat(
-    parameters: {
-      model: "gpt-4o-mini",
-      messages: [
-        { role: "user", content: message }
-      ],
-      temperature: 0.7,
-  })
-  response.dig("choices", 0, "message", "content")
+    response = @client.chat(
+      parameters: {
+        model: "gpt-3.5-turbo",
+        messages: [
+          { role: "user", content: message }
+        ],
+        temperature: 0.7,
+    })
+    response.dig("choices", 0, "message", "content")
 
   end
 end
