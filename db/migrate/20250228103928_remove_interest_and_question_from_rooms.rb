645c2caf128c3e7465546213b90101ccbe90e259
amy huang <flower850603@gmail.com>
2025-04-02 04:35:37 +0900

    remove turbo_frame wrapped link_to since that stops turbo stream from working

--- db/migrate/20250228103928_remove_interest_and_question_from_rooms.rb
+++
@@ -1,6 +0,0 @@
-class RemoveInterestAndQuestionFromRooms < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :rooms, :interest, :string
-    remove_column :rooms, :question, :string
-  end
-end
--- test/models/active_user_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class ActiveUserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
